1496594350c8158c0355db02368871e5e75d745f
Armaan Gupta <armaan@guppy.im>
2025-07-07 08:42:39 -0400

    Initial Commit

---
+++ SideChat/SideChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideChatApp.swift
+//  SideChat
+//
+//  Created by Armaan Gupta on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SideChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SideChatTests/SideChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  SideChatTests.swift
+//  SideChatTests
+//
+//  Created by Armaan Gupta on 7/7/25.
+//
+
+import Testing
+@testable import SideChat
+
+struct SideChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
